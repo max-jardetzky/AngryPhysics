b3f6ca30cb211f3d3288a2f28bfedf0861a7ad63
max-jardetzky <max.jardetzky20@bcp.org>
2018-03-29 09:41:19 -0700

    first commit

--- SelectionSort_4Kajargnibo.java
+++ SelectionSort_4Kajargnibo.java
@@ -1,7 +1,7 @@
 // Presentation Project
 // by Darius Kafayi, Max Jardetzky, and Jack Gnibus
 
-public class AngryPhysics_4Kajargnibo {
+public class SelectionSort_4Kajargnibo {
    public static void main(String[] args) {
    
    }
